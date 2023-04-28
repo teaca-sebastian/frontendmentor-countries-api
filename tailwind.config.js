86b42d9835a0655945b67b44bd8bfb6e03542c2d
Teaca Sebastian <sebiteaca@gmail.com>
2023-04-28 10:06:29 +0300

    added custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				body: ['Nunito']
+			},
+			colors: {
+				blue: {
+					text: 'hsl(200, 15%, 8%)',
+					bg: 'hsl(207, 26%, 17%)',
+					body: 'hsl(209, 23%, 22%)'
+				},
+				gray: {
+					text: 'hsl(0, 0%, 52%)',
+					bg: 'hsl(0, 0%, 98%)',
+				},
+				white: 'hsl(0, 0%, 100%)'
+			}
+		},
 	},
 	plugins: [],
   darkMode: 'class'
